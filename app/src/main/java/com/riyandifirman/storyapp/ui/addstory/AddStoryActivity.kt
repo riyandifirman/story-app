74b3f55a7d5aebc074116a6517eec8dd8cb9101e
riyandifirman <riyandi.firman08@gmail.com>
2023-05-10 23:38:27 +0700

    finishing add story page layout

---
+++ app/src/main/java/com/riyandifirman/storyapp/ui/addstory/AddStoryActivity.kt
@@ -0,0 +1,12 @@
+package com.riyandifirman.storyapp.ui.addstory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riyandifirman.storyapp.R
+
+class AddStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_story)
+    }
+}
