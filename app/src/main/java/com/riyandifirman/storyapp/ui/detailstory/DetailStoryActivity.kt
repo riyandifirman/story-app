10a9b11949637a76a7857911377a36ec26bc8f43
riyandifirman <riyandi.firman08@gmail.com>
2023-05-10 22:50:47 +0700

    finishing detail story page layout

---
+++ app/src/main/java/com/riyandifirman/storyapp/ui/detailstory/DetailStoryActivity.kt
@@ -0,0 +1,12 @@
+package com.riyandifirman.storyapp.ui.detailstory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riyandifirman.storyapp.R
+
+class DetailStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_story)
+    }
+}
