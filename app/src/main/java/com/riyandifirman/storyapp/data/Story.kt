c92701a777d117724ad922f7c789807cf76ae40c
riyandifirman <riyandi.firman08@gmail.com>
2023-05-11 10:53:03 +0700

    add responses from API

---
+++ app/src/main/java/com/riyandifirman/storyapp/data/Story.kt
@@ -0,0 +1,13 @@
+package com.riyandifirman.storyapp.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Story(
+    val id: String,
+    val name: String,
+    val description: String,
+    val photoUrl: String,
+    val date: String
+): Parcelable
---
+++ app/src/main/java/com/riyandifirman/storyapp/response/AddStoryResponse.kt
@@ -0,0 +1,12 @@
+package com.riyandifirman.storyapp.response
+
+import com.google.gson.annotations.SerializedName
+
+data class AddStoryResponse(
+
+	@field:SerializedName("error")
+	val error: Boolean,
+
+	@field:SerializedName("message")
+	val message: String
+)
