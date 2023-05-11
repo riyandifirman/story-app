0d7371db2f3249c3b9980b3610407318802a94ce
riyandifirman <riyandi.firman08@gmail.com>
2023-05-11 22:41:11 +0700

    finishing logic in detail story activity

--- app/src/main/java/com/riyandifirman/storyapp/ui/detailstory/DetailStoryActivity.kt
+++ app/src/main/java/com/riyandifirman/storyapp/ui/detailstory/DetailStoryActivity.kt
@@ -2,11 +2,33 @@ package com.riyandifirman.storyapp.ui.detailstory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.riyandifirman.storyapp.R
+import com.riyandifirman.storyapp.databinding.ActivityDetailStoryBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DetailStoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailStoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_story)
+        binding = ActivityDetailStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvDetailName.text = intent.getStringExtra("list_name")
+
+        val dateString = intent.getStringExtra("list_date")
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+        val date = inputFormat.parse(dateString)
+        val outputFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+        val formattedDate = outputFormat.format(date)
+        binding.tvDate.text = formattedDate
+
+        binding.tvDetailDescription.text = intent.getStringExtra("list_description")
+        Glide.with(this)
+            .load(intent.getStringExtra("list_image"))
+            .into(binding.ivDetailPhoto)
     }
 }
